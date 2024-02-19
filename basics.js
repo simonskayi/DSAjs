d75edf7a02c5d17798bbb9a5456f52ae2c1da43e
simonskayi <arhinsimons@gmail.com>
2024-02-19 19:46:38 +0000

    added new codes

---
+++ basics.js
@@ -0,0 +1,10 @@
+//Manupulating Array
+
+let arrayI = ['ama','Esi', 'Joe']
+
+console.log(arrayI.pop())
+console.log(arrayI.push("Joy"))
+console.log(arrayI)
+console.log(arrayI.unshift("JW.ORG"))
+console.log(arrayI)
+console.log(arrayI.shift())
