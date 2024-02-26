1ab9a02afa9b4367829d4fb2931c18ed2d85c289
simonskayi <arhinsimons@gmail.com>
2024-02-26 11:47:47 +0000

    new dsa practice

--- twoPointer.js
+++ twoPointer.js
@@ -1,3 +1,24 @@
-let numbers = [4,3,10,5,6,8,11]
-let target = 11
+let numbers = [1,2,4,5,6,8,11,15,18]
+let target = 14
 
+let leftPointer = 0
+let rightPointer= numbers.length - 1
+
+function pointer(numbers , target){
+
+    while(leftPointer <= rightPointer){
+
+    let sum = numbers[leftPointer] + numbers[rightPointer]
+    if(sum === target){
+        return [leftPointer,rightPointer]
+    }
+    else if(sum < target){
+        leftPointer++
+    }
+    else {
+        rightPointer--
+    }
+}
+}
+
+console.log(pointer(numbers,target))
