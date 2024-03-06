6a8dd6acde6ba11980977338d5c425b310a8c373
simonskayi <arhinsimons@gmail.com>
2024-03-06 07:58:32 +0000

    bubbleSort

--- twoPointer.js
+++ twoPointer.js
@@ -19,6 +19,28 @@ function pointer(numbers , target){
         rightPointer--
     }
 }
+};
+
+function bubbleSort(arr){
+   let swap
+    do {  
+        swap = false;
+    
+         for(let i=0; i < arr.length -1; i++){
+            if(arr[i] > arr[i+1]){
+                let temp = arr[i]
+               arr[i] = arr[i+1]
+               arr[i+1]= temp
+                swap = true
+            }
+            console.log(arr)
+        }
+    }
+    while (swap){
+        return arr
+    }
+   
 }
 
 console.log(pointer(numbers,target))
+console.log(bubbleSort([9,4,5,3,6,10,1,2,7,6]))
